b9f2d5f1c3ecd050b157a19dac0cb56c88cda34f
heitor.amaral <heitor.amaral90@outlook.com>
2020-06-28 15:06:27 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/hamaral/wschecklist/WsChecklistApplication.java
@@ -0,0 +1,13 @@
+package br.com.hamaral.wschecklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsChecklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsChecklistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hamaral/wschecklist/WsChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hamaral.wschecklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsChecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
